55b74bd51b063cd8f5bf3b98f12cec1366e80493
Spartak2 <orkotop030@gmail.com>
2024-03-06 02:25:27 +0200

    timer and snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', event => {
+    const dalay = Number(form.dalay.value);
+
+    event.preventDefault();
+  const promise = new Promise((resolve, reject) => {
+    if (form.state.value === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (form.state.value === 'rejected') {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+  promise
+    .then(dalay => {
+            iziToast.show({
+            title: 'Success',
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topCenter',
+            color: 'green',
+        });
+    })
+    .chatch (dalay => {
+        iziToast.show({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+        color: 'red',
+    });
+});
+
+event.currentTarget.reset();
+});
