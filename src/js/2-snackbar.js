44de90c2ff69a3566a3aabaa4da4dc9927808702
Spartak2 <orkotop030@gmail.com>
2024-03-09 19:36:35 +0200

    timer

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,37 +3,37 @@
 const form = document.querySelector('.form')
 
 form.addEventListener('submit', event => {
-    const dalay = Number(form.dalay.value);
+    const delay = Number(form.delay.value);
 
-  event.preventDefault();
+    event.preventDefault();
     const promise = new Promise((resolve, reject) => {
-      if (form.state.value === 'fulfilled') {
-        setTimeout(() => {
-          resolve(delay);
-        }, delay);
-      } else if (form.state.value === 'rejected') {
-        setTimeout(() => {
-          reject(delay);
-        }, delay);
-      }
+        if (form.state.value === 'fulfilled') {
+            setTimeout(() => {
+                resolve(delay);
+            }, delay);
+        } else if (form.state.value === 'rejected') {
+            setTimeout(() => {
+                reject(delay);
+            }, delay);
+        }
     });
-  promise
-    .then(dalay => {
+    promise
+        .then(delay => {
             iziToast.show({
-            title: 'Success',
-            message: `✅ Fulfilled promise in ${delay}ms`,
-            position: 'topCenter',
-            color: 'green',
-          });
-    })
-    .chatch (dalay => {
-        iziToast.show({
-        title: 'Error',
-        message: `❌ Rejected promise in ${delay}ms`,
-        position: 'topCenter',
-        color: 'red',
-      });
-    });
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                position: 'topCenter',
+                color: 'green',
+            });
+        })
+        .catch(delay => {
+            iziToast.show({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+                position: 'topCenter',
+                color: 'red',
+            });
+        });
 
-  event.currentTarget.reset();
+    event.currentTarget.reset();
 });
